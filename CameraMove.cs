cb4d5a0c8a5c4e16c671dde40f2c56e0eb578352
nats0 <53441826+nats0@users.noreply.github.com>
2021-12-13 02:13:57 -0800

    Update CameraMove.cs

--- CameraMove.cs
+++ CameraMove.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class CameraMove : MonoBehaviour
 {
+    // Variables
     public Transform player;
     Vector3 offset;
+    
     // Start is called before the first frame update
     void Start()
     {
